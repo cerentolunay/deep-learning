1ce0bed2259962cb323d0b7c198c9da1a8656c14
cerentolunay <cerentlny@gmail.com>
2026-05-17 09:58:11 +0300

    lstm.py tamamlandı.

--- 04-lstm/lstm.py
+++ 04-lstm/lstm.py
@@ -45,7 +45,7 @@ class LSTM(nn.Module):
         super(LSTM, self).__init__()# bir üst sınıfın constructor'ını çağırma
         self.embedding = nn.Embedding(vocab_size, embedding_dim) # embedding katmanı
         self.lstm = nn.LSTM(embedding_dim, hidden_dim) # lstm katmanı
-        self.fc = nnLinear(hidden_dim, vocab_size) # tam bağlantılı katman
+        self.fc = nn.Linear(hidden_dim, vocab_size) # tam bağlantılı katman
 
     def forward(self, x):    # ileri besleme fonksiyonu
         
@@ -103,7 +103,7 @@ for emb_size, hidden_size, lr in product(embeding_sizes, hidden_sizes, learning_
         best_loss = total_loss
         best_params = {'embedding_dim': emb_size, 'hidden_dim': hidden_size, 'learning_rate': lr}
     print()    
-print("En İyi Parametreler: {best_params}")
+print(f"En İyi Parametreler: {best_params}")
 
 
 
@@ -112,7 +112,7 @@ print("En İyi Parametreler: {best_params}")
 
 #%% lstm training 
 final_model= LSTM(len(vocab), best_params['embedding_dim'], best_params['hidden_dim'])
-optimizer=optim.Adam(final_model.parameters(), lr = best_params['learing_rate'])
+optimizer=optim.Adam(final_model.parameters(), lr = best_params['learning_rate'])
 loss_function = nn.CrossEntropyLoss() # kayıp fonksiyonu 
 print("final model training")
 epochs =100
@@ -134,5 +134,24 @@ for epoch in range(epochs):
 
 #%% test ve degerlendirme evolution
 
+# kelime tahmini fonsiyonu : başlangıc kelimesini ve n adet kelime uretmesini sağla 
+def predict_sequence(start_word, num_words):
+    current_word = start_word
+    output_sequence = [current_word] # çıktı dizisi
 
+    for _ in range(num_words):# belirtilen sayıda kelime tahmini
+        with torch.no_grad(): #gradyan hesaplaması yapmadan
+            input_tensor=prepare_sequence([current_word], word_to_ix)# kelimeden tensor dönüşümü 
+            output = final_model(input_tensor)
+            predicted_idx = torch.argmax(output).item() # en yüksek olasılıga sahip kelimenin indexi
+            predicted_word = ix_to_word[predicted_idx] # indexe karsılık gelen kelimeyi return eder
+            output_sequence.append(predicted_word)
+            current_word = predicted_word # bir sonraki tahmin için mevcut kelimeşeri güncelle 
+    return output_sequence # tahmn edilen kelime dizisi return edilir
 
+start_word = "ürün"
+num_predictions= 1
+predict_sequence= predict_sequence(start_word, num_predictions)
+print ("".join(predict_sequence))
+
+# %%
